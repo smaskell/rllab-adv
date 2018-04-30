c0a7224056c2ea39e5ad5c173f57505c30e86394
Samuel Maskell <samuelmaskell@gmail.com>
2018-04-30 04:29:31 -0400

    Add option to use fear when training with adversary

--- adversarial/scripts/train_adversary.py
+++ adversarial/scripts/train_adversary.py
@@ -140,7 +140,8 @@ for ne in range(n_exps):
         discount=0.995,
         gae_lambda=gae_lambda,
         step_size=step_size,
-        is_protagonist=True
+        is_protagonist=True,
+        use_danger=args.danger
     )
 
     ## Optimizer for the Adversary ##
